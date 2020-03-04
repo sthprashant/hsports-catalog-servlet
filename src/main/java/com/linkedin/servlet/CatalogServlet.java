e8d20458695f8c9e8ee622bd2db46d630c6ad2e9
Prashant Shrestha <sthprashant@gmail.com>
2020-03-04 21:41:56 +1100

    initial commit:

---
+++ src/main/java/com/linkedin/servlet/CatalogServlet.java
@@ -0,0 +1,77 @@
+package com.linkedin.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CatalogServlet
+ */
+@WebServlet(urlPatterns = "/CatalogServlet",asyncSupported = true)
+public class CatalogServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CatalogServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append(request.getParameter("name"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String manufacturer = request.getParameter("manufacturer");
+		String sku = request.getParameter("sku");
+		
+		response.setHeader("someHeader", "someHeaderValue");
+		response.addCookie(new Cookie("someCookie", "someCookieValue"));
+		
+		Catalog.addItem(new CatalogItem(name, manufacturer,sku));
+		
+		request.setAttribute("items", Catalog.getItems());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+		dispatcher.forward(request, response);
+	}
+
+//	private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//		
+//	}
+//	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
